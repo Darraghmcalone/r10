1b341b114b79678056f2d8cadf0d57e32065055b
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-08-30 16:58:00 +0100

    add title to about route

--- js/navigation/RootStackNavigator.js
+++ js/navigation/RootStackNavigator.js
@@ -1,7 +1,15 @@
+import React, { Component } from 'react';
 import { createStackNavigator } from 'react-navigation';
+import { Text } from 'react-native';
 import { AboutContainer } from '../screens/About';
 
-const RootStack = createStackNavigator({
-    About: AboutContainer
-});
+const RootStack = createStackNavigator(
+    {
+        About: {
+            screen: AboutContainer,
+            navigationOptions: {
+                title: 'About',
+            },
+        }
+    });
 export default RootStack;
