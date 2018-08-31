23bcca67c379b66c83a91a1e6752818b66dbffc3
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-08-31 12:57:30 +0100

    add header with gradient and title

--- js/navigation/NavigationLayout.js
+++ js/navigation/NavigationLayout.js
@@ -3,6 +3,7 @@ import {
     createStackNavigator,
     createBottomTabNavigator
 } from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { AboutContainer } from '../screens/About';
 import { colours, fonts } from '../config/styles';
 import { FavesContainer } from '../screens/Faves';
@@ -10,13 +11,17 @@ import { ScheduleContainer } from '../screens/Schedule';
 import { SessionContainer } from '../screens/Session';
 import { SpeakerContainer } from '../screens/Speaker'
 import { MapContainer } from '../screens/Map'
+import { sharedNavigationOptions } from './config';
 
 const AboutStack = createStackNavigator(
     {
         About: AboutContainer,
     },
     {
-        headermode: 'none'
+        navigationOptions: ({ navigation }) => ({
+            title: navigation.state.routeName,
+            ...sharedNavigationOptions(navigation)
+        })
     }
 );
 
@@ -26,7 +31,10 @@ const FavesStack = createStackNavigator(
         Faves: FavesContainer,
     },
     {
-        headermode: 'none'
+        navigationOptions: ({ navigation }) => ({
+            title: navigation.state.routeName,
+            ...sharedNavigationOptions(navigation)
+        })
     }
 );
 
@@ -35,7 +43,10 @@ const ScheduleStack = createStackNavigator(
         Schedule: ScheduleContainer,
     },
     {
-        headermode: 'none'
+        navigationOptions: ({ navigation }) => ({
+            title: navigation.state.routeName,
+            ...sharedNavigationOptions(navigation)
+        })
     }
 );
 
@@ -44,7 +55,10 @@ const SessionStack = createStackNavigator(
         Session: SessionContainer,
     },
     {
-        headermode: 'none'
+        navigationOptions: ({ navigation }) => ({
+            title: navigation.state.routeName,
+            ...sharedNavigationOptions(navigation)
+        })
     }
 );
 
@@ -52,30 +66,50 @@ const SpeakerStack = createStackNavigator(
     {
         Speaker: SpeakerContainer
     },
-    {
-        headermode: 'none'
-    }
+
 );
 const MapStack = createStackNavigator(
     {
         Map: MapContainer
-    },
-    {
-        headermode: 'none'
+    }, {
+        navigationOptions: ({ navigation }) => ({
+            title: navigation.state.routeName,
+            ...sharedNavigationOptions(navigation)
+        })
     }
 );
 // Dedicated stacks for Schedule and Faves will go here too!
 
 export default createBottomTabNavigator(
     {
-        About: { screen: AboutStack },
-        Faves: { screen: FavesStack },
         Schedule: { screen: ScheduleStack },
-        Session: { screen: SessionStack },
-        Speaker: { screen: SpeakerStack },
-        Map:{ screen: MapStack }
+        Map: { screen: MapStack },
+        Faves: { screen: FavesStack },
+        About: { screen: AboutStack },
     },
     {
+        navigationOptions: ({ navigation }) => ({
+            ...sharedNavigationOptions(navigation),
+            tabBarIcon: ({ focused, tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName;
+                if (routeName === 'About') {
+                    iconName = `ios-information-circle`;
+                } else if (routeName === 'Faves') {
+                    iconName = `ios-heart`;
+                } else if (routeName === 'Map') {
+                    iconName = `ios-map`;
+                } else if (routeName === 'Schedule') {
+                    iconName = `ios-calendar`;
+                }
+
+
+                // You can return any component that you like here! We usually use an
+                // icon component from react-native-vector-icons
+                return <Ionicons name={iconName} size={25} color={tintColor} />;
+            },
+        }),
+
         tabBarOptions: {
             activeTintColor: colours.white,
 
--- js/navigation/RootStackNavigator.js
+++ js/navigation/RootStackNavigator.js
@@ -1,9 +1,12 @@
 import { createStackNavigator } from 'react-navigation';
 import NavigationLayout from './NavigationLayout';
-
 const RootStack = createStackNavigator(
     {
         Layout: NavigationLayout
     },
+    {
+        mode: 'modal',
+        headerMode: 'none'
+    }
 );
-export default RootStack;
+export default RootStack
