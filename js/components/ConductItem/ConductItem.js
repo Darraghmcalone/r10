ac5e9031232e5f475bc7fbab26f8a5ef746327fb
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-09-16 17:00:36 +0100

    implement toggle animation for code of conduct

--- js/components/ConductItem/ConductItem.js
+++ js/components/ConductItem/ConductItem.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text } from 'react-native';
+import { View, Text, LayoutAnimation, Animated, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
 
 export default class ConductItem extends Component {
+    constructor() {
+        super();
+        this.state = {
+            open: false,
+            rotate: new Animated.Value(0)
+        }
+    }
     render() {
         const { itemData } = this.props;
 
         return (
             <View>
-                <Text style={styles.conductHeader}>{itemData.title}</Text>
-                <Text style={styles.conductText}>{itemData.description}</Text>
+                <TouchableOpacity
+                    onPress={() => {
+                        LayoutAnimation.easeInEaseOut();
+
+                        this.state.open
+                            ? Animated.timing(this.state.rotate, {
+                                toValue: 0,
+                                duration: 200
+                            }).start(() => this.setState({ open: !this.state.open }))
+                            : Animated.timing(this.state.rotate, {
+                                toValue: 1,
+                                duration: 300
+                            }).start(() => this.setState({ open: !this.state.open }));
+                    }}
+                    style={{ flexDirection: "row" }}
+                >
+                    <Text style={styles.conductHeader}>{itemData.title}</Text>
+                </TouchableOpacity>
+
+                <Text style={[styles.conductText, { height: this.state.open ? "auto" : 0 }]}>{itemData.description}</Text>
             </View>
         );
     }
+
 }
 
 ConductItem.propTypes = {
