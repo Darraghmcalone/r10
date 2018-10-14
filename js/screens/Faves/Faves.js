e04596e6d7cc544aed7013982735510e62bb39df
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-10-14 22:46:52 +0100

    insert favedata to faves screen; add conditional text if no faves have been added

--- js/screens/Faves/Faves.js
+++ js/screens/Faves/Faves.js
@@ -2,16 +2,11 @@ import React from "react";
 import { StatusBar, ScrollView, Text } from "react-native";
 import SessionList from "../../components/SessionList";
 import styles from "./styles";
-const Faves = ({ sessions, navigation, faveIds }) => {
+const Faves = ({ allSessions, faveIds }) => {
   return (
     <ScrollView>
-      <StatusBar barStyle="light-content" />
-      {sessions.length > 0 ? (
-        <SectionLists
-          sessions={sessions}
-          navigation={navigation}
-          faveIds={faveIds}
-        />
+      {allSessions.length > 0 ? (
+        <SessionList sessionData={allSessions} faveIds={faveIds} />
       ) : (
         <Text>Add some Favorites to see them here!</Text>
       )}
--- js/screens/Faves/FavesContainer.js
+++ js/screens/Faves/FavesContainer.js
@@ -1,66 +1,46 @@
 import React, { Component } from "react";
 import Faves from "./Faves";
-import FavesContext from "../../context/FavesContext";
-import { Query } from "react-apollo";
+import Loader from "../../components/Loader";
 import gql from "graphql-tag";
-import { Text, ActivityIndicator, View } from "react-native";
+import { Query } from "react-apollo";
+import { formatSessionData } from "../../lib/helpers";
+import FavesContext from "../../context/FavesContext";
+
+const allSessionsQuery = gql`
+  query {
+    allSessions {
+      startTime
+      title
+      location
+      id
+      description
+      speaker {
+        id
+      }
+    }
+  }
+`;
 
 export default class FavesContainer extends Component {
-  static navigationOptions = {
-    title: "Faves"
-  };
   render() {
     return (
-      <Query
-        query={gql`
-          {
-            allSessions {
-              startTime
-              location
-              title
-              id
-            }
+      <Query query={allSessionsQuery}>
+        {({ loading, data }) => {
+          if (loading || !data) {
+            return <Loader />;
           }
-        `}
-      >
-        {({ loading, error, data: { allSessions } }) => {
-          if (loading)
-            return (
-              <View style={{ alignItems: "center", justifyContent: "center" }}>
-                <ActivityIndicator size="large" />
-              </View>
-            );
-          if (error) return <Text>Error :(</Text>;
-
           return (
             <FavesContext.Consumer>
               {values => {
-                const favesIdArr = [];
-                values.faveIds.map(item => favesIdArr.push(item.id));
-                const favedSessions = allSessions.filter(session =>
-                  favesIdArr.includes(session.id)
+                const allFaves = [];
+                values.faveIds.map(item => allFaves.push(item.id));
+                const filteredSessionIds = data.allSessions.filter(session =>
+                  allFaves.includes(session.id)
                 );
-
-                let sessions = favedSessions
-                  .reduce((acc, curr) => {
-                    const timeExists = acc.find(
-                      section => section.title === curr.startTime
-                    );
-                    timeExists
-                      ? timeExists.data.push(curr)
-                      : acc.push({
-                          title: curr.startTime,
-                          data: [curr]
-                        });
-                    return acc;
-                  }, [])
-                  .sort((a, b) => a.title - b.title);
-
                 return (
                   <Faves
-                    navigation={this.props.navigation}
-                    faveIds={favesIdArr}
-                    sessions={sessions}
+                    allSessions={formatSessionData(filteredSessionIds)}
+                    faveIds={values.faveIds}
                   />
                 );
               }}
